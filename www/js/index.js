91bdc4f7ac7305afc4c58f16debe70e56fafc238
yong <yong@asial.co.jp>
2019-07-30 19:24:02 +0900

    feat: get uuid for electron and mobile device

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,33 @@ var app = {
 
     onDeviceReady: function() {
         console.log('device ready');
+        this.getDeviceInfo();
+    },
+
+    getDeviceUuid: function() {
+        try {
+            const machineIdSync = require('node-machine-id').machineIdSync;
+            return machineIdSync({original: true});
+        } catch (error) {
+            console.error(error);
+            return 'Could not get machine id';
+        }
+    },
+
+    getDeviceInfo: function() {
+        const platformId = window.cordova.platformId;
+        const deviceInfo = document.getElementById('device-info');
+        let uuid = null;
+        if (['electron'].indexOf(platformId) >= 0) {
+            // get uuid from npm package
+            uuid = this.getDeviceUuid();
+        } else if (device && device.uuid && ['ios', 'android'].indexOf(platformId) >= 0) {
+            // get uuid from cordova-plugin-device
+            uuid = device.uuid;
+        } else {
+            uuid = `The ${platformId} platform is not supported.`;
+        }
+        if (uuid) deviceInfo.innerHTML = `Device UUID: ${uuid}`;
     }
 };
 
